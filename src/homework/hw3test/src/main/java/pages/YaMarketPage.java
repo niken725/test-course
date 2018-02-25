efc1193ab2fd759d70a57de65278869d742b97da
Ekaterina Pavlova <Niken725@gmail.com>
2018-02-25 18:42:57 +0300

    hw3_all

--- src/homework/hw3test/src/main/java/pages/YaMarketPage.java
+++ src/homework/hw3test/src/main/java/pages/YaMarketPage.java
@@ -4,63 +4,101 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-public class YaMarketPage {
+public class YaMarketPage  extends BasePage{
+
+    //private static BasePage basePage;
+
     private By searchInput = By.cssSelector("#header-search");
     private By searchButton = By.cssSelector(".button2");
-    private By searchResultField = By.cssSelector(".n-snippet-card2");
+    private By searchContentField = By.cssSelector(".n-snippet-card2");
     private By sortPriceField = By.xpath("//a[text()=\"по цене\"]");
-    private By sortAscField = By.cssSelector(".n-filter-sorter_sort_asc");
+    private By sortPriceIconField = By.cssSelector(".n-filter-sorter.n-filter-sorter_state_select");
     private By priceField = By.cssSelector(".n-snippet-card2__main-price .price");
 
 
     private final WebDriver driver;
     public YaMarketPage(WebDriver driver) {
+        super(driver);
         this.driver = driver;
     }
 
-    public void search(String text) {
-        driver.findElement(searchInput).sendKeys(text);
-        driver.findElement(searchButton).click();
-
+    public void searchContent (String text) {
+        sendKeysByElement(searchInput, text);
+        clickByElement(searchButton);
     }
 
-    public Integer getResult() {
-        return driver.findElements(searchResultField).size();
+    public Integer getContentResult() {
+        return driver.findElements(searchContentField).size();
     }
 
-    public void sortPrice() {
-        driver.findElement(sortPriceField).click();
-
+    public void sortPriceAsc() {
+        clickByElement(sortPriceField);
     }
-    public void priceRe() {
-        //return driver.findElement(priceField).getText();
-        List<WebElement> prices = driver.findElements(priceField);
-        prices = parsePrices.;
-        ArrayList<Integer> prices = parsePrices();
+
+    public String sortIconResult() {
+        driver.navigate().refresh();
+        return driver.findElement(sortPriceIconField).getAttribute("class");
+    }
+
+    public  void sortPriceDesc() {
+        clickByElement(sortPriceField);
+        clickByElement(sortPriceField);
+    }
+
+    public Boolean sortPriceAscResult () {
+        driver.navigate().refresh();
+        List<WebElement> priceElements = driver.findElements(priceField);
+        ArrayList<Integer> prices = parsePrices(priceElements);
+        int i = 1;
+        Boolean sortAscResult = true;
+        while (i < prices.size()){
+            Integer element = prices.get(i-1);
+            Integer nextElement = prices.get(i);
+            if (element <= nextElement) {
+                sortAscResult = true;
+            }
+            else {sortAscResult = false;
+            break;}
+            i++;
+        }
+        return sortAscResult;
+    }
+
+    public Boolean sortPriceDescResult () {
+        driver.navigate().refresh();
+        List<WebElement> elements = driver.findElements(priceField);
+        ArrayList<Integer> prices = parsePrices(elements);
+        int i = 1;
+        Boolean sortDescResult = true;
+        while (i < prices.size()){
+            Integer element = prices.get(i-1);
+            Integer nextElement = prices.get(i);
+            if (element >= nextElement) {
+                sortDescResult = true;
+            }
+            else {sortDescResult = false;
+                break;}
+            i++;
+        }
+        return sortDescResult;
     }
 
     protected ArrayList<Integer> parsePrices(List<WebElement> prices) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
+        ArrayList<Integer> convertResult = new ArrayList<Integer>();
 
         prices.forEach(element -> {
             String rawPrice = element.getText();
             rawPrice = rawPrice.replaceAll("[^\\d]", "");
             Integer price = Integer.parseInt(rawPrice);
-            result.add(price);
+            convertResult.add(price);
         });
-
-        return result;
+        return convertResult;
     }
 
 
 
-    public void priceSt() {
-        //Alert(priceRe().substring(0,priceRe().length()-1));
-       //return priseI = driver.findElements(priceField).toArray();
-       // String st = new String((driver.findElement(priceField)).getText());
-    }
+
 }
--- src/homework/hw3test/src/test/java/TestCourse/MarketTest.java
+++ src/homework/hw3test/src/test/java/TestCourse/MarketTest.java
@@ -20,24 +20,42 @@ public class MarketTest extends BaseTest {
     @Test
     public void marketTest() {
         driver.get("https://market.yandex.ru/");
-        yaMarketPage.search("холодильник");
-
-        int result = yaMarketPage.getResult();
+        yaMarketPage.searchContent("холодильник");
+        int result = yaMarketPage.getContentResult();
         Assert.assertEquals(12, result);
     }
 
     @Test
-    public void sortTest() {
+    public void sortPriceAscIconTest() {
         driver.get("https://market.yandex.ru/");
-        yaMarketPage.search("холодильник");
-        yaMarketPage.sortPrice();
+        yaMarketPage.searchContent("холодильник");
+        yaMarketPage.sortPriceAsc();
+        Assert.assertTrue( yaMarketPage.sortIconResult().contains("asc"),"true");
     }
 
     @Test
-    public void oneTest() {
+    public void sortPriceAscTest () {
         driver.get("https://market.yandex.ru/");
-        yaMarketPage.search("холодильник");
-        yaMarketPage.sortPrice();
-        yaMarketPage.priceRe();
+        yaMarketPage.searchContent("холодильник");
+        yaMarketPage.sortPriceAsc();
+        boolean sortAscResult = yaMarketPage.sortPriceAscResult();
+        Assert.assertEquals(true, sortAscResult);
+    }
+
+    @Test
+    public void sortPriceDescIconTest() {
+        driver.get("https://market.yandex.ru/");
+        yaMarketPage.searchContent("холодильник");
+        yaMarketPage.sortPriceDesc();
+        Assert.assertTrue(yaMarketPage.sortIconResult().contains("desc"), "true");
+    }
+
+    @Test
+    public void sortPriceDescTest() {
+        driver.get("https://market.yandex.ru/");
+        yaMarketPage.searchContent("холодильник");
+        yaMarketPage.sortPriceDesc();
+        boolean sortDescResult = yaMarketPage.sortPriceDescResult();
+        Assert.assertEquals(true, sortDescResult);
     }
 }
