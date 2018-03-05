8fab9c8bf19a84d48edcd9108f3c1aa3625f356e
Ekaterina Pavlova <niken725@gmail.com>
2018-03-05 15:54:28 +0300

    05/03-3

--- src/test/java/TestCourse/BaseTest.java
+++ src/test/java/TestCourse/BaseTest.java
@@ -17,7 +17,7 @@ public class BaseTest {
         //System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
         //driver.manage().window().maximize();
-       // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @AfterClass
