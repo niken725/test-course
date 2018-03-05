07c81a37edf7317b9e11ce2f59556cc3ec24b7b3
Ekaterina Pavlova <niken725@gmail.com>
2018-03-05 16:00:16 +0300

    05/03-4

--- src/test/java/TestCourse/BaseTest.java
+++ src/test/java/TestCourse/BaseTest.java
@@ -13,8 +13,8 @@ public class BaseTest {
 
     @BeforeClass
     public static void setup() {
-        System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
-        //System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
         //driver.manage().window().maximize();
        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
